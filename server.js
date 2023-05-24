4bf11634aaee5b874b94b7f53cdf09e082611fd1
Shobhit Jain <104783891+bjshobhit@users.noreply.github.com>
2023-05-24 10:27:31 +0000

    abc

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
